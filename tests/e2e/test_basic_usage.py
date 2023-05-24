4561ec2f914a66177a3aeaf53b4e953fea3f8bff
Ian Sillitoe <ian@sillit.com>
2023-05-24 18:24:59 +0100

    normalise newlines

--- tests/e2e/test_basic_usage.py
+++ tests/e2e/test_basic_usage.py
@@ -4,7 +4,6 @@ End to end test to check the basic usage
 import os
 import subprocess
 from pathlib import Path
-import time
 
 REPO_ROOT = Path(__file__).parent.parent.parent.resolve()
 DATA_DIR = REPO_ROOT / "example_files"
@@ -16,25 +15,30 @@ def test_basic_usage(tmp_path):
     example_structure_path = DATA_DIR / f"{af_id}.pdb"
     expected_output = f"""
 chain_id	domain_id	chopping	uncertainty
-AF-Q5T5X7-F1-model_v4	domain_1	189-237	0.028
-AF-Q5T5X7-F1-model_v4	domain_2	238-353	0.0
-AF-Q5T5X7-F1-model_v4	domain_3	381-505	0.0
-AF-Q5T5X7-F1-model_v4	domain_4	524-658	0.0
-AF-Q5T5X7-F1-model_v4	domain_5	687-826	0.0
+AF-Q5T5X7-F1-model_v4	domain_1	189-237	0.0221
+AF-Q5T5X7-F1-model_v4	domain_2	238-353	0.0221
+AF-Q5T5X7-F1-model_v4	domain_3	381-505	0.0221
+AF-Q5T5X7-F1-model_v4	domain_4	524-658	0.0221
+AF-Q5T5X7-F1-model_v4	domain_5	687-826	0.0221
 """.strip()
     orig_path = Path.cwd()
     script_path = REPO_ROOT / "get_predictions.py"
 
-    # make sure we can run this in a temp directory
-    stdout = None
-    stderr = None
+    # make sure we can run this in an isolated directory
+    results_output = None
     try:
         os.chdir(str(tmp_path))
         # os.chdir(str(orig_path))
         cmd_args = ["python", str(script_path), "--structure_file", str(example_structure_path)]
+        print("CWD: " + str(tmp_path))
+        print("CMD: " + " ".join(cmd_args))
         completed_process = subprocess.run(cmd_args, check=True, capture_output=True)
-        stdout = completed_process.stdout.decode()
-        stderr = completed_process.stderr.decode()
+        results_output = completed_process.stdout.decode().strip()
+    except subprocess.CalledProcessError as e:
+        print(f"ERROR: CMD: {e.cmd}")
+        print(f"ERROR: STDOUT: {e.stdout.decode()}")
+        print(f"ERROR: STDERR: {e.stderr.decode()}")
+        raise
     finally:
         os.chdir(str(orig_path))
 
@@ -42,4 +46,8 @@ AF-Q5T5X7-F1-model_v4	domain_5	687-826	0.0
     assert completed_process.returncode == 0
     # check the output file was created
     # check the output file has the expected content
-    assert stdout.strip() == expected_output
+    assert normalise_output(results_output) == normalise_output(expected_output)
+
+
+def normalise_output(output_str):
+    return output_str.replace("\r\n", "\n")
