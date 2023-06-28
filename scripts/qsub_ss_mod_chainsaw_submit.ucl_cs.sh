343224548c7f266b2fda662a394285d61c27f871
Jude Wells <judewells@hotmail.com>
2023-06-28 16:57:23 +0100

    mod submit script

--- scripts/qsub_ss_mod_chainsaw_submit.ucl_cs.sh
+++ scripts/qsub_ss_mod_chainsaw_submit.ucl_cs.sh
@@ -3,7 +3,7 @@
 #$ -l h_vmem=7G
 #$ -l h_rt=40:00:00
 #$ -l scratch0free=10G
-#$ -L tscratch=10G
+#$ -l tscratch=10G
 #$ -P cath
 #$ -S /bin/bash
 #$ -N chsaw-ss_mod
@@ -11,6 +11,7 @@
 #$ -o ./logs
 #$ -j y
 #$ -t 1-18892:1000
+#$ -wd /SAN/cath/cath_v4_3_0/alphafold/chainsaw_on_alphafold/chainsaw
 cat "$0"
 cd /SAN/cath/cath_v4_3_0/alphafold/chainsaw_on_alphafold/chainsaw
 bash scripts/ss_mod_chainsaw_submit.ucl_cs.sh
