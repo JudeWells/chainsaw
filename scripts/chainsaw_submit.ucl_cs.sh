e948801b2c3cee8179cb1105dd83b5d8126827c4
Ian Sillitoe <ian@sillit.com>
2023-07-06 00:29:33 +0100

    process lists of zip files

--- scripts/chainsaw_submit.ucl_cs.sh
+++ scripts/chainsaw_submit.ucl_cs.sh
@@ -6,8 +6,9 @@
 # usage:
 #   qsub -t 1-16933 chainsaw_submit.ucl_cs.sh
 #
-# Assumes index file has been split into chunks e.g. "zipindex.00000001":
-#   split -a 8 -d 1 -l 1000 data/zipindex zipindex.
+# Each job will expect to find an equivalent file containing a list of zipfiles,
+# created by 
+#   split -a 8 -d 1 -l 1000 all_proteome_zipfiles.txt data/zipfiles.
 #
 
 
@@ -19,16 +20,14 @@ if [[ "$SGE_TASK_ID" == "undefined" ]]; then
 fi
 
 CATH_VERSION=v4_3_0
-ZIP_DIR=/SAN/bioinf/afdb_domain/zipfiles
 HOME_DIR=/home/$USER
 SHARED_REPO="${SHARED_REPO:-${SGE_O_WORKDIR}/chainsaw}"
-ZIP_EXTRACT=$SHARED_REPO/scripts/zip_extract.py
 SCRATCH_DIR=/scratch0/$USER
 LOCAL_TASK_DIR=$SCRATCH_DIR/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
 PYTHON_EXE=${SGE_O_WORKDIR}/venv/bin/python3
 
 # assumes the zip index file has been split into chunks (named 'zipindex.00000001')
-ZIP_INDEX_FILE=${SGE_O_WORKDIR}/data/zip_index.`printf "%08d" $SGE_TASK_ID`
+ZIPFILES_LIST_FILE=${SGE_O_WORKDIR}/data/zipfiles.`printf "%08d" $SGE_TASK_ID`
 RESULTS_FILE=${SGE_O_WORKDIR}/results/$(basename $ZIP_INDEX_FILE).results.csv
 
 if [ ! -e "${PYTHON_EXE}" ]
@@ -37,9 +36,9 @@ then
     exit 1
 fi
 
-if [ ! -e "${ZIP_INDEX_FILE}" ]
+if [ ! -e "${ZIPFILES_LIST_FILE}" ]
 then
-    echo "ERROR: input index file '${ZIP_INDEX_FILE}' does not exist"
+    echo "ERROR: input list file '${ZIPFILES_LIST_FILE}' does not exist"
     exit 1
 fi
 
@@ -49,7 +48,7 @@ then
     exit 1
 fi
 
-echo "INDEX_FILE         : $ZIP_INDEX_FILE"
+echo "ZIPFILES_LIST_FILE : $ZIPFILES_LIST_FILE"
 echo "RESULTS_FILE       : $RESULTS_FILE"
 echo "DATE_STARTED       : "`date`
 echo
@@ -70,21 +69,25 @@ echo "Loading python ..."
 module load python/3.8.5
 echo "...DONE"
 
-echo "Extracting PDBs ..."
+echo "Extracting PDBs ... "
+echo $(date)
 cd $LOCAL_TASK_DIR
 LOCAL_PDB_DIR=$LOCAL_TASK_DIR/pdb
 mkdir -p $LOCAL_PDB_DIR
-/usr/bin/time $ZIP_EXTRACT -i $ZIP_INDEX_FILE -z $ZIP_DIR -o $LOCAL_PDB_DIR 
-echo "...DONE"
+cat $ZIPFILES_LIST_FILE | xargs -I XXX sh -c unzip -d $LOCAL_PDB_DIR
+echo $(date)
+echo "...DONE "
 
 echo "Running chainsaw ..."
-/usr/bin/time $PYTHON_EXE $SHARED_REPO/get_predictions.py --structure_directory $LOCAL_PDB_DIR -o $RESULTS_FILE
-echo
+echo $(date)
+$PYTHON_EXE $SHARED_REPO/get_predictions.py --structure_directory $LOCAL_PDB_DIR -o $RESULTS_FILE
+echo $(date)
+echo "...DONE"
 
 echo "Removing local temp dir ..."
 rm -rf $LOCAL_TASK_DIR
-echo
+echo "...DONE"
 
-echo "DATE_FINISHED   : "`date`
+echo "DATE_FINISHED   : " $(date)
 echo "JOB_COMPLETE"
 
